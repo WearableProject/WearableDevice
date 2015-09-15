3dafa0be7e2c99a95a2e25bc6bf88c99e0568b0c
Godley <charlotte.godley@hotmail.co.uk>
2015-09-16 00:06:07 +0100

    Fixed a lot of bugs using platformio which were mainly syntax errors or problems which I didn't understand with C++ and the fast spi library. Just needs testing but it should now be ready to go

---
+++ Pearl/Pearl.cpp
@@ -0,0 +1,18 @@
+#include <FastSPI_LED2.h>
+#include <Pearl.h>
+
+Pearl::Pearl() : CFastSPI_LED2()
+{
+  addLeds(new WS2811Controller800Khz<data_pin>(), colours, leds, 0);
+  setBrightness(40);
+}
+
+void Pearl::Change(int LED, CRGB colour)
+{
+  colours[LED] = colour;
+}
+
+void Pearl::Update()
+{
+  show();
+}
--- Pearl/Pearl.h
+++ Pearl/Pearl.h
@@ -1,13 +1,13 @@
 #include "Arduino.h"
 #include <Wire.h>
 #include <MMA7660.h>
-#include "FastSPI_LED2.h"
+#include <FastSPI_LED2.h>
 #include <SoftwareSerial.h>
 
-class Pearl: public FastLED {
+class Pearl : public CFastSPI_LED2 {
 private:
   static const int leds = 6;
-  int data_pin;
+  static const int data_pin = 4;
   CRGB colours[leds];
 public:
   Pearl();
